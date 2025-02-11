0f29b98a8e6fa518a6a4b2b316e816a12edbf105
thien20 <thienhtt20@gmail.com>
2025-02-11 10:30:22 +0700

    added 1 column named date to ingest and scrape

--- pipeline/ingest.py
+++ pipeline/ingest.py
@@ -2,55 +2,52 @@ import os
 import json
 import mysql.connector
 
-from config import *
+import config
+
 from mysql.connector import connection
 
-db_config = {
-    'user': DB_USER,
-    'password': DB_PASSWORD,
-    'host': DB_HOST,
-    'database': DB_NAME
-}
 
-# Connect to the database
-try:
-    # print("hello 1")
-    conn = connection.MySQLConnection(**db_config)
-    cursor = conn.cursor()
-    
-except mysql.connector.Error as e:
-    print(f"Failed to connect to MySQL: {e}")
-    exit(1)
+db_config = config.Config("config.json").db_config
+def ingest():
 
-# Data files
-# data_dir = r'D:\MY_FOLDER\Project\vuaBE\data'
-data_dir = r'./data'
-
-table = 'news'
-
-with open(os.path.join(data_dir, 'all_data.json'), 'r', encoding='utf-8') as f:
-    data = json.load(f)
-    records_to_insert = []
-    for record in data:
-        record_id = record.get('id')
-        link = record.get('link', '')
-        title = record.get('title', '')
-        content = record.get('content', '')
-        category = record.get('category', '')
-        if record_id is None or not link or not title or not content:
-            print(f"Skipping invalid record: {record}")
-            continue
-        records_to_insert.append((record_id, link, title, content, category))
-
+    # Connect to the database
     try:
-        cursor.executemany(f"""
-        INSERT INTO `{table}` (id, link, title, content, category)
-        VALUES (%s, %s, %s, %s, %s)
-        """, records_to_insert)
+        conn = connection.MySQLConnection(**db_config)
+        cursor = conn.cursor()
+        
     except mysql.connector.Error as e:
-        print(f"Error inserting records into table `{table}`: {e}")
+        print(f"Failed to connect to MySQL: {e}")
+        exit(1)
 
-# Commit and close connection
-conn.commit()
-cursor.close()
-conn.close()
+    # Data files
+    data_dir = r'./pipeline/data'
+
+    table = 'news'
+
+    with open(os.path.join(data_dir, 'all_data.json'), 'r', encoding='utf-8') as f:
+        data = json.load(f)
+        records_to_insert = []
+        for record in data:
+            record_id = record.get('id')
+            link = record.get('link', '')
+            title = record.get('title', '')
+            content = record.get('content', '')
+            category = record.get('category', '')
+            date = record.get('date', '')
+            if record_id is None or not link or not title or not content:
+                print(f"Skipping invalid record: {record}")
+                continue
+            records_to_insert.append((record_id, link, title, content, category, date))
+
+        try:
+            cursor.executemany(f"""
+            INSERT INTO `{table}` (id, link, title, content, category, date)
+            VALUES (%s, %s, %s, %s, %s, %s)
+            """, records_to_insert)
+        except mysql.connector.Error as e:
+            print(f"Error inserting records into table `{table}`: {e}")
+
+    # Commit and close connection
+    conn.commit()
+    cursor.close()
+    conn.close()
--- pipeline/scrape.py
+++ pipeline/scrape.py
@@ -1,87 +1,44 @@
-from bs4 import BeautifulSoup
-
-import requests
-import json
 import os
+import json
 
-global_id = 0 
+from utils.file_utils import ensure_dir, read_json, write_json
+from utils.scrape_utils import scrape_data_by_cate
 
-def process_p_contents(p_contents):
-    return " ".join(p_contents)
-
-def scrape_data_by_cate(url):
-    global global_id
-    
-    # Fetch and parse the main category page
-    response = requests.get(url)
-    if response.status_code != 200:
-        print(f"Failed to fetch URL: {url}, Status Code: {response.status_code}")
-        return []
-
-    html_content = response.text
-    soup = BeautifulSoup(html_content, 'html.parser')
-    h3_tags = soup.find_all('h3')
-    
-    # Store the scraped data
-    scraped_data = []
-
-    for tag in h3_tags:
-        a_tag = tag.find('a')
-        if a_tag:
-            link = a_tag.get('href')
-            title = a_tag.get('title')
-            
-            # Fetch and parse the linked page
-            try:
-                response_title = requests.get(link)
-                if response_title.status_code != 200:
-                    print(f"Failed to fetch linked page: {link}, Status Code: {response_title.status_code}")
-                    continue
-                
-                title_soup = BeautifulSoup(response_title.text, 'html.parser')
-                
-                # Find all <p> tags with class "Normal"
-                p_tags = title_soup.find_all('p', class_='Normal')
-                p_contents = [p_tag.text.strip() for p_tag in p_tags]
-                
-                content = process_p_contents(p_contents)
-
-                scraped_data.append({
-                    'id': global_id,
-                    'link': link,
-                    'title': title,
-                    'content': content,
-                    'category': url.split('/')[-1].replace('-', '_')
-                })
-                global_id += 1
-
-            except Exception as e:
-                print(f"Error fetching or parsing linked page: {link}, Error: {e}")
-                continue
-       
-    return scraped_data
-
-if __name__ == '__main__':
+def scrape_all_categories():
     original_url = 'https://vnexpress.net'
-    data_dir = r'D:\MY_FOLDER\Project\vuaBE\data'
-    
-    # Take the categories from the vuacrawl.json file
-    # category = ['thoi-su', 'the-gioi', 'kinh-doanh', 'giai-tri', 'the-thao', 'phap-luat', 'giao-duc', 'suc-khoe', 'doi-song', 'du-lich', 'khoa-hoc', 'so-hoa', 'oto-xe-may', 'y-kien', 'tam-su']    
-    with open(r'D:\MY_FOLDER\Project\vuaBE\vuacrawl.json', encoding='utf-8') as f:
-        data = json.load(f)
-        categories = [item['share_url'] for item in data] # results: ['/thoi-su', '/goc-nhin', '/the-gioi']
-    
+
+    # Define relative paths
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    # print("This is current dir", script_dir)
+    data_dir = os.path.join(script_dir, 'data')
+    # print("This is data dir", data_dir)
+    json_path = r'../vuaBE/vuacrawl.json'
+
+    # Ensure data directory exists
+    ensure_dir(data_dir)
+
+    # Load categories
+    categories = read_json(json_path)
+    categories = [item['share_url'] for item in categories]
+
+    # Remove video category and truncate "/"
     categories.remove('https://video.vnexpress.net')
     url_list = [original_url + cate for cate in categories]    
-    categories = [cate.split('/')[-1] for cate in categories] # truncate '\' for matching folder name
-    
+    categories = [cate.split('/')[-1] for cate in categories] 
+
     all_data = []
-    for i, temp_url in enumerate(url_list):
+    for temp_url in url_list:
         data = scrape_data_by_cate(temp_url)
         all_data.extend(data)
         print("Done scraping data for category:", temp_url)
     
-    with open(f"{data_dir}/all_data.json", 'w', encoding='utf-8') as f:
-        json.dump(all_data, f, ensure_ascii=False, indent=2)
-    
-    print("Done scraping data for all categories")
+    # Save all scraped data
+    write_json(all_data, os.path.join(data_dir, 'all_data.json'))
+    print("Done scraping and writing data for all categories")
+
+    return all_data
+
+
+# if __name__ == '__main__':
+#     scrape_all_categories()
+    # consume_kafka_messages()
